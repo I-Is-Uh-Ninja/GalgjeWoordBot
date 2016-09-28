37addcf197e15455ee24426f1220aa8123d1456d
Ian <iannijhuis@hotmail.com>
2016-09-28 11:29:23 +0200

    Woorden array werkt in netbeans

--- GalgjeWoordBot/src/main/java/ai/WoordlijstProcessor.java
+++ GalgjeWoordBot/src/main/java/ai/WoordlijstProcessor.java
@@ -5,13 +5,19 @@
  */
 package ai;
 
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Deze klasse zorgt voor het inlezen van een text-file met woorden, en het zoeken naar een woord in de file
@@ -21,20 +27,42 @@ import java.util.Scanner;
  */
 public class WoordlijstProcessor {
     private static File woordenlijst;
+    private int aantalWoorden;
+    private String[] woorden;
 
     public WoordlijstProcessor() {
-        woordenlijst = new File("dictionary.txt");
+        woordenlijst = new File("woordenlijst.txt");
+        aantalWoorden = telWoorden();
+        woorden = zetWoordenInArray(aantalWoorden);
+    }
+
+    public int getAantalWoorden() {
+        return this.aantalWoorden;
+    }
+
+    public void setAantalWoorden(int aantalWoorden) {
+        this.aantalWoorden = aantalWoorden;
+    }
+
+    public String[] getWoorden() {
+        return this.woorden;
+    }
+
+    public void setWoorden(String[] woorden) {
+        this.woorden = woorden;
     }
     
     //tool om een korte woordenlijst te genereren, die alleen woorden heeft van min tot max aantal letters
     protected static void decreaseList(File file, int min, int max){
-        try(Scanner input = new Scanner(file); PrintWriter fw = new PrintWriter(new BufferedWriter(new FileWriter("woordenlijst.txt")));){
+        try(Scanner input = new Scanner(file); FileWriter fw = new FileWriter("woordenlijst.txt");){
             while(input.hasNextLine()){
                 String woord = input.nextLine();
                 woord = woord.trim();
                 if(woord.matches("\\w{" + min + "," + max + "}")){//als het woord bestaat uit min tot max aantal letters (exclusief letters met speciale tekens, zoals een trema)
+                    System.out.println("Woord gevonden: " + woord);
                     String newline = String.format("%n");//omdat verschillende OS'en een andere definitie hebben van een newline, dient men deze methode te gebruiken
                     fw.write(woord + newline);
+                    fw.flush();
                 }
             }
         }
@@ -45,8 +73,8 @@ public class WoordlijstProcessor {
             System.err.println("IOException: " + ex.getMessage());
         }
     }
-  //tel het aantal woorden in de woordenlijst
-    int telWoorden(){
+    //tel het aantal woorden in de woordenlijst
+    private int telWoorden(){
         int aantalWoorden = 0;
         try(Scanner input = new Scanner(woordenlijst)){
             while(input.hasNextLine()){
@@ -59,6 +87,28 @@ public class WoordlijstProcessor {
         }
         return 0;
     }
+    
+    private String[] zetWoordenInArray(int aantalWoorden){
+    	System.out.println("Het aantal woorden is: " + aantalWoorden);
+    	String[] woorden = new String[aantalWoorden];
+    	try(Scanner input = new Scanner(woordenlijst)){
+    		int index = 0;
+            while(input.hasNextLine()){
+                woorden[index] = input.nextLine();
+                index++;
+            }
+        } catch (FileNotFoundException ex) {
+            System.err.println("Bestand niet gevonden: " + ex.getMessage());
+        }
+    	return woorden;
+    }
+    
+    public String kiesRandomWoord(){
+    	int randomIndex = (int) (Math.random() * aantalWoorden);//dit bepaald het random woord, de int die gegenereerd is staat voor het # van het woord in de lijst
+    	return woorden[randomIndex];
+    }
+    
+    /*
     //zoek een woord op een bepaald lijnnummer (dit is nodig voor een random woord kiezen)
     public String zoekWoord(int lijnNummer){
         try(Scanner input = new Scanner(woordenlijst);){
@@ -82,4 +132,5 @@ public class WoordlijstProcessor {
             return null;
         }
     }
+    */
 }
--- GalgjeWoordBot/src/test/java/ai/WoordlijstProcessorTest.java
+++ GalgjeWoordBot/src/test/java/ai/WoordlijstProcessorTest.java
@@ -1,12 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ai;
 
 import java.io.File;
-
 import junit.framework.TestCase;
 
-public class WoodlijstProcessorTest extends TestCase {
-
-	public void testDecreaseList() {
+/**
+ *
+ * @author Ian
+ */
+public class WoordlijstProcessorTest extends TestCase {
+    
+    public void testDecreaseList() {
 		File dictionary = new File("dictionary.txt");
 		if(dictionary.exists()){
 			WoordlijstProcessor.decreaseList(dictionary, 3, 12);
@@ -20,7 +28,7 @@ public class WoodlijstProcessorTest extends TestCase {
 	
 	public void testTelWoorden(){
 		WoordlijstProcessor processor = new WoordlijstProcessor();
-		int aantalWoorden = processor.telWoorden();
+		int aantalWoorden = processor.getAantalWoorden();
 		System.out.println("Aantal woorden: " + aantalWoorden);
 		assertTrue(aantalWoorden > 0);
 	}
@@ -31,5 +39,5 @@ public class WoodlijstProcessorTest extends TestCase {
 		assertFalse(randomWoord.equals(""));
 		System.out.println("Random woord is: " + randomWoord);
 	}
-
+    
 }
